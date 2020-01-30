e207ff63850ef520eb07bcd57ae9b28f650f9ea3
Kat Sullivan <kmsullivan012@gmail.com>
2020-01-29 22:58:28 -0500

    add first repo

---
+++ Week01/IntroCPP/myFirstCpp.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+
+// this function asks the user for a user, it also returns a bool representing if the user's guess was correct or incorrect
+// it takes the answer as a parameter
+bool makeAGuess(int answer)
+{
+    int userGuess;
+    
+    cout << "Guess a number : ";
+    cin >> userGuess;
+    cout << "your guess is " << userGuess << endl;
+
+    if (userGuess < answer)
+    {
+        cout << "you're too low" << endl;
+        return false;
+    }
+    else if (userGuess > answer)
+    {
+        cout << "you're too high!" << endl;
+        return false;
+    }
+    // if the answer is not smaller than the answer or larger than the answer, the user guessed correctly
+    else
+    {
+        cout << "You're doing amazing...you won my heart" << endl;
+        return true;
+    }
+
+}
+
+int main()
+{
+    // create new random sequence based on time of day 
+    srand(time(NULL));  
+
+    // this will generate a random number between 1 and 10
+    // a different number will be generated each game
+    // because it is declared outside of any function it is a global variable
+    // const means the value of this variable will not be changed
+    const int answer = rand() % 10 + 1;
+    // cout << "answer " << answer << endl;
+
+    // a variable letting us know if the user guessed corrected
+    bool userCorrect = false;
+
+    cout << "Let's play a game!" << endl;
+
+    // keep guessing until user guesses correctly
+    while (userCorrect == false)
+    {
+        userCorrect = makeAGuess(answer);
+    }
+
+    // let the user know they won the game
+    cout << "You won the game!" << endl;
+
+
+    return 0;
+}
